cf4d32abfa60bb6cb6501f9d86c783c2cdf84c3b
SantiagoVila <vilasantiago16@gmail.com>
2025-07-31 19:51:39 -0300

    prueba

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL,
+  // Añadimos /api al final de la URL base
+  baseURL: `${import.meta.env.VITE_API_URL}/api`,
 });
 
 export default api;
